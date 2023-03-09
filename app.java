98338e1c4f3ffb1a9c82ae453148a34b5b778188
Cloud User <abiola.meraiyebu@gmail.com>
2023-03-08 22:14:42 -0500

    modified

--- app.java
+++ app.java
@@ -2,6 +2,15 @@ This is great
 Developing java app
 .net applications
 NodeJS application
+<<<<<<< HEAD
 python application
 web application
 bug removed
+great
+This app is excellent
+This app is excellent for all users
+[development 4482191] excellent app for all
+ 1 file changed, 1 insertion(+)
+This app is excellent
+This app is excellent for all users
+>>>>>>> 0a510fd (excellent app for all)
